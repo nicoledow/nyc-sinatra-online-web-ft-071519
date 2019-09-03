b0b17aadf2c27884ef1cdd81e99fd208e4082849
Nicole Dow <nikki.dow71@gmail.com>
2019-09-03 00:36:22 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,24 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  set :views, proc { File.join(root, '../views/') }
+  
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"/application/new_figure"
+  end
+  
+  post '/figures' do
+    #binding.pry
+    @figure = Figure.new(params['figure'])
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
 end
